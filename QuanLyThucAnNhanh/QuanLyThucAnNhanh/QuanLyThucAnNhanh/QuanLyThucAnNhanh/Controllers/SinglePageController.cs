236c8e5c307000e4affc304075466761e4cd196d
Web react with Hao <phamthehao1208@gmail.com>
2023-01-05 14:24:50 +0700

    up file

---
+++ QuanLyThucAnNhanh/QuanLyThucAnNhanh/QuanLyThucAnNhanh/QuanLyThucAnNhanh/Controllers/SinglePageController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using QuanLyThucAnNhanh.Models;
+
+
+
+namespace QuanLyThucAnNhanh.Controllers
+{
+    public class SinglePageController : Controller
+    {
+        QuanLyFastFoodEntities db = new QuanLyFastFoodEntities();
+        // GET: SinglePage
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult SingleProduct(int ? ma)
+        {
+
+            ViewBag.sanpham = db.SanPhams.Where(x => x.Ma == ma).FirstOrDefault();
+            ViewBag.LoaiSP = db.LoaiSPs.ToList();
+            var mal = db.SanPhams.Where(x => x.Ma == ma).FirstOrDefault().MaLoai;
+            ViewBag.SPCL = db.SanPhams.Where(x => x.MaLoai == mal).ToList();
+            ViewBag.BESTSELL = db.SanPhams.OrderByDescending(x => x.Ma).Take(5).ToList();
+            return View();
+        }
+    }
+}
